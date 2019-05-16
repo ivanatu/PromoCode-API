06579b600ad02f49f1eff64f73fa8c9947f1edae
ivanatu <ivanatu@gmail.com>
2019-05-16 18:02:56 +0300

    changes to init file

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize application
+promoapp = Flask(__name__, static_folder=None)
+TEST = True
+
+promoapp.config['DEBUG'] = True
+if os.environ.get('DATABASE_URL'):
+    promoapp.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+else:
+    if TEST:
+        promoapp.config['SQLALCHEMY_DATABASE_URI2'] = os.environ['SQLALCHEMY_DATABASE_URI2']
+    else:
+        promoapp.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+
+promoapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+promoapp.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+db = SQLAlchemy(promoapp)
+db.init_app(promoapp)
+
+from . import views
+
